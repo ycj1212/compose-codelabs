ecfd16f2b04da8258c33f5ae90271603ef9889c8
Yang Cheol Joo <rlaengks0001@naver.com>
2022-03-14 10:52:26 +0900

    Complete learning <4. Tweaking the UI>

--- app/src/main/java/com/ycj1212/composecodelabs/MainActivity.kt
+++ app/src/main/java/com/ycj1212/composecodelabs/MainActivity.kt
@@ -3,13 +3,14 @@ package com.ycj1212.composecodelabs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ycj1212.composecodelabs.ui.theme.ComposeCodelabsTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,13 +18,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeCodelabsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                Greeting("Android")
             }
         }
     }
@@ -31,7 +26,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(color = MaterialTheme.colors.primary) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    }
 }
 
 @Preview(showBackground = true)
