52aab9fbf2936522830f278d982bf0f8908aab4b
Yang Cheol Joo <rlaengks0001@naver.com>
2022-03-15 20:21:43 +0900

    Complete learning <11. Animating your list>

--- app/src/main/java/com/ycj1212/composecodelabs/MainActivity.kt
+++ app/src/main/java/com/ycj1212/composecodelabs/MainActivity.kt
@@ -3,6 +3,9 @@ package com.ycj1212.composecodelabs
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -78,7 +81,13 @@ private fun Greetings(names: List<String> = List(1000) { "$it" }) {
 private fun Greeting(name: String) {
     val expanded = remember { mutableStateOf(false) }
 
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    val extraPadding by animateDpAsState(
+        if (expanded.value) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
 
     Surface(
         color = MaterialTheme.colors.primary,
@@ -86,8 +95,8 @@ private fun Greeting(name: String) {
     ) {
         Row(modifier = Modifier.padding(24.dp)) {
             Column(modifier = Modifier
-                .weight(1f)
-                .padding(bottom = extraPadding)
+                    .weight(1f)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello, ")
                 Text(text = name)
